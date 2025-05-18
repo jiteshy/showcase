5376f2f1561f12317aae703b7b2bc01880e0f40a
Jitesh Yadav <jiteshyadav1990@gmail.com>
2025-05-18 12:06:59 -0400

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
